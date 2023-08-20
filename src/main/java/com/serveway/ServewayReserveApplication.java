37f756442454136505110f6b7eed22b78cac7b39
jhw216 <jhw216@naver.com>
2023-08-20 22:38:12 +0900

    v1

---
+++ src/main/java/com/serveway/ServewayReserveApplication.java
@@ -0,0 +1,13 @@
+package com.serveway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServewayReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServewayReserveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serveway/ServewayReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serveway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServewayReserveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
